d830fccbb9904879ac9befa6b2fa62b59f4e4419
jing <newzou9@gmail.com>
2016-12-31 21:03:18 -0500

    track simple objects in videos
    Added a basic background subtractor.
    Able to track object in a basic way.

--- track.py
+++ track.py
@@ -1,31 +1,46 @@
-import numpy as np
 import cv2
+import numpy as np
+from manager import WindowManager, CaptureManager
 
-cap = cv2.VideoCapture(0)
-fgbg = cv2.BackgroundSubtractorMOG()
+class Track(object):
+    def __init__(self):
+        self._windowManager = WindowManager('Track', self.onKeypress)
+        self._captureManager = CaptureManager(cv2.VideoCapture(0), self._windowManager, True)
+        self._fgbg = cv2.BackgroundSubtractorMOG()
 
-while(1):
-    ret, frame = cap.read()
+    def run(self):
+        """Run the main loop."""
+        self._windowManager.createWindow()
+        while self._windowManager.isWindowCreated:
+            self._captureManager.enterFrame()
+            frame = self._captureManager.frame
 
-    if ret == True:
-        original = np.copy(frame)
-
-        fgmask = fgbg.apply(frame)
-
-        ret, thresh = cv2.threshold(fgmask,60 ,255,0)
-
-        contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+            original = np.copy(frame)
+            fgmask = self._fgbg.apply(frame)
+            ret, thresh = cv2.threshold(fgmask,60 ,255,0)
+            contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
    
-        for c in contours:
-            if cv2.contourArea(c) > 1000:
-                cv2.drawContours(original, c, -1, (0,255,0), 3)
+            for c in contours:
+                if cv2.contourArea(c) > 1000:
+                    cv2.drawContours(frame, c, -1, (0,255,0), 3)
 
-        cv2.imshow('image', original)
-        
-    k = cv2.waitKey(30) & 0xff
-    # ESC to close
-    if k == 27:
-        break
+            self._captureManager.exitFrame()
+            self._windowManager.processEvents()
 
-cap.release()
-cv2.destroyAllWindows()
+    def onKeypress (self, keycode):
+        """Handle a keypress. 
+        space  -> Take a screenshot.
+        tab    -> Start/stop recording a screencast.
+        escape -> Quit."""
+        if keycode == 32: # space
+            self._captureManager.writeImage('screenshot.png')
+        elif keycode == 9: # tab
+            if not self._captureManager.isWritingVideo:
+                self._captureManager.startWritingVideo('screencast.avi')
+            else:
+                self._captureManager.stopWritingVideo()
+        elif keycode == 27: # escape
+            self._windowManager.destroyWindow()
+
+if __name__=="__main__":
+    Track().run()
